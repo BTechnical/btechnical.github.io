5fddfa9bd892d72ef0397604ce148c4800fb77c1
BTechnical <36489466+BTechnical@users.noreply.github.com>
2024-05-01 00:38:09 -0400

    additional features in songs.js

--- songs.bundle.js
+++ songs.bundle.js
@@ -1,1 +1,1 @@
-$("#myTable").DataTable({ajax:"/songs.txt",columns:[{data:"Title"},{data:"Artist"},{data:"Year"},{data:"Duo"},{data:"Explicit"},{data:"SongId"}]});
+$("#myTable").DataTable({ajax:"/songs.txt",columns:[{data:"Title"},{data:"Artist"},{data:"Year"},{data:"Duo"},{data:"Explicit"},{data:"SongId"}],order:[[0,"asc"]],columnDefs:[{target:5,visible:!1,searchable:!1}],lengthMenu:[25,50,100,250],return:!0});
